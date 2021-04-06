f368103fcc9cb8e1ef5d64e9942bf15b6dc96c67
frame-creator <65874705+frame-creator@users.noreply.github.com>
2021-04-06 09:31:27 +0900

    nodejs 에러핸들링 추가

--- nodejs__backend/app.js
+++ nodejs__backend/app.js
@@ -4,23 +4,36 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 
+const projectsRoutes = require('./routes/projects-routes');
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+const HttpError = require('./models/http-error');
 
-  next();
-});
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(express.static(path.join('public')));
+
+//app.use((req, res, next) => {
+//  res.setHeader('Access-Control-Allow-Origin', '*');
+//  res.setHeader(
+//    'Access-Control-Allow-Headers',
+//    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//  );
+//  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+//  next();
+//});
 
 app.use('/api/projects', projectsRoutes);
 app.use((req, res, next) => {
-  const error = new HttpError('해당 경로를 찾을 수 없습니다.', 404);
-  throw error;
- });
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
+//app.use((req, res, next) => {
+//  const error = new HttpError('해당 경로를 찾을 수 없습니다.', 404);
+//  throw error;
+// });
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
