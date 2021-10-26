d5d26a389c21b67dfb36a398e0632ee8d0533c84
frame-creator <65874705+frame-creator@users.noreply.github.com>
2021-10-26 21:38:53 +0900

    반응형 디자인 사이드 드로어 메뉴 컴포넌트 추가 & 미디어 쿼리 적용

--- react_frontend/src/elements/navigation/MainNavigaton.js
+++ react_frontend/src/elements/navigation/MainNavigaton.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, {useState}  from 'react';
 import {Link} from 'react-router-dom';
 import Logo from '../../images/port.png'
 
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
 import './MainNavigation.css';
+import SideDrawer from './SideDrawer';
 
 const MainNavigation = props => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawerHandler = () => {
+    setDrawerIsOpen(true) ;
+  }
+
+  const closeDrawerHandler = () => {
+    setDrawerIsOpen(false);
+  }
     return (
+
+      <React.Fragment>
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
         <MainHeader>
-         
+        <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
+          <span />
+          <span />
+          <span />
+        </button>
          
       <ul className="main-links">
         <Link to="/"><li class="logo-box ">
@@ -17,7 +38,7 @@ const MainNavigation = props => {
            
        </li></Link>
        <li className="main-navigation__name">
-        <h2> Developer Kwon Koo Yon </h2>
+        <h2>Developer Koo Yon Kwon</h2>
         </li>
         </ul>
       
@@ -27,6 +48,7 @@ const MainNavigation = props => {
       </nav>  
 
         </MainHeader>
+        </React.Fragment>
     );
 };
 
